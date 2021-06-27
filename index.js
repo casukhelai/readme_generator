7ad40e15913b6604a34b9b7d63ecf354743a1de9
Ishya Casukhela <casukhelai@gmail.com>
2021-06-27 12:26:32 -0400

    made markdown template

--- index.js
+++ index.js
@@ -3,7 +3,65 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Add Table of Contents somewhere here
+const questions = [
+    // Project Title
+    {
+        type: "input",
+        message: "",
+        name: "projectTitle",
+    },
+    // Enter Description
+    {
+        type: "input",
+        message: "",
+        name: "description",
+    },
+    // Installation instructions
+    {
+        type: "input",
+        message: "",
+        name: "installInstruct",
+    },
+    // Usage information
+    {
+        type: "input",
+        message: "",
+        name: "usageInfo",
+    },
+    // contribution guidelines
+    {
+        type: "input",
+        message: "",
+        name: "contriGuide",
+    },
+    // test instructions
+    {
+        type: "input",
+        message: "",
+        name: "testInstruct",
+    },
+    // License...change input to options(?)
+    // add license badge to top of readme and notice is added to the section of the readme entitled license that explains which license the app is covered under
+    {
+        type: "input",
+        message: "",
+        name: "licenses",
+    },
+    // Github username
+    {
+        type: "input",
+        message: "",
+        name: "github",
+    },
+    // email
+    {
+        type: "input",
+        message: "",
+        name: "email",
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
---
+++ readmeTEMP.md
@@ -0,0 +1,19 @@
+<!-- # ${projectTitle}
+
+## Description
+${question.description}
+
+## Installation Instructions
+${question.installInstruct}
+
+## Usage Information
+${question.usageInfo}
+
+## Contribution Guidelines
+${question.contriGuide}
+
+## Test Instruction
+${question.testInstruct}
+
+## Questions
+If you have any further questions please contact me at ${email}. View more of my work at ${github} -->
