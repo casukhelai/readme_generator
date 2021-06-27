24133b68490354a31860d7fcd3744bc371785706
Ishya Casukhela <casukhelai@gmail.com>
2021-06-27 13:00:43 -0400

    updated question block in index.js

--- index.js
+++ index.js
@@ -8,56 +8,64 @@ const questions = [
     // Project Title
     {
         type: "input",
-        message: "",
+        message: "What is the name of your project?",
         name: "projectTitle",
     },
     // Enter Description
     {
         type: "input",
-        message: "",
+        message: "Please provide a short description of your project.",
         name: "description",
     },
     // Installation instructions
     {
         type: "input",
-        message: "",
+        message: "What command should be run to install any dependencies?",
         name: "installInstruct",
     },
     // Usage information
     {
         type: "input",
-        message: "",
+        message: "How is this application meant to be used?",
         name: "usageInfo",
     },
     // contribution guidelines
     {
         type: "input",
-        message: "",
+        message: "What are the contribution guidelines for your project?",
         name: "contriGuide",
     },
     // test instructions
     {
         type: "input",
-        message: "",
+        message: "What command should be run for testing?",
         name: "testInstruct",
     },
     // License...change input to options(?)
     // add license badge to top of readme and notice is added to the section of the readme entitled license that explains which license the app is covered under
     {
-        type: "input",
-        message: "",
+        type: "list",
+        message: "What license should your README have?",
+        choices: [
+            "MIT",
+            "GNU v3",
+            "Apache 2.0",
+            "BSD 3-Clause",
+            "Mozilla Public License 2.0",
+            "Unlicense"
+        ],
         name: "licenses",
     },
     // Github username
     {
         type: "input",
-        message: "",
+        message: "Enter your GitHub username:",
         name: "github",
     },
     // email
     {
         type: "input",
-        message: "",
+        message: "Enter your email: ",
         name: "email",
     },
 
