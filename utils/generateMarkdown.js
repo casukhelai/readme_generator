653953e1c6da85f348b414058e6f5418d2bdb3a4
Ishya Casukhela <casukhelai@gmail.com>
2021-06-27 15:20:55 -0400

    added license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,6 +12,7 @@ function renderLicenseBadge(license) {
   if (license in badge){
     return `[![License: ${license}](${badge})](${renderLicenseLink})`;
   } else 
+  // if unlicensed
     return "";
 }
 
@@ -25,14 +26,17 @@ function renderLicenseLink(license) {
     "Apache 2.0" : "https://opensource.org/licenses/Apache-2.0",
     "Mozilla Public License 2.0" : "https://opensource.org/licenses/MPL-2.0"
   }
-
-  
-  
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // This is the block of text about the license
+  if(license != "Unlicense"){
+    return `##Licence
+    This repository is under the [${license}](${renderLicenseLink(license)}).`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
